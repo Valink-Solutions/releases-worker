20ac545e908ebc962332cea75fd9016282fcc86e
JakePIXL <jakewjevans@gmail.com>
2023-11-14 22:01:49 -0500

    fixed KV errors

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use chrono::{DateTime, FixedOffset};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct GitHubRelease {
     tag_name: String,
     published_at: String,
@@ -13,20 +13,20 @@ struct GitHubRelease {
     assets: Vec<GitHubAsset>,
 }
 
-#[derive(Serialize, Deserialize, Debug)] 
+#[derive(Serialize, Deserialize, Debug, Default)] 
 struct GitHubAsset {
     name: String,
     browser_download_url: String,
     download_count: i64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct TotalDownloads {
     total_downloads: i64,
     updated_at: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct RecentRelease {
     version: String,
     pub_date: String,
@@ -51,53 +51,52 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 }
 
 async fn get_total_downloads(_req: worker::Request, ctx: RouteContext<()>) -> Result<Response> {
+    let kv = ctx.kv("KV_CHUNKVAULT_DOWNLOADS");
 
-    let kv = ctx.kv("KV_CHUNKVAULT_DOWNLOADS")?;
-
-    let old_downloads: TotalDownloads = match kv.get("recent_download_count").json::<TotalDownloads>().await? {
-        Some(value) => {
-            value
-        },
-        None => return Response::error("Failed to fetch recent download count", 500),
+    let old_downloads = if let Ok(kv) = &kv {
+        kv.get("recent_download_count").json::<TotalDownloads>().await.ok().unwrap()
+    } else {
+        None
     };
 
-    // If the value is older than 5 minutes, return it else fetch a new value
-    let updated_at = DateTime::parse_from_rfc3339(
-        old_downloads.updated_at.as_str(),
-    ).map_err(|_| "Failed to parse updated_at")?;
+    let updated_at = match &old_downloads {
+        Some(downloads) => DateTime::parse_from_rfc3339(&downloads.updated_at).unwrap_or_else(|_| DateTime::<FixedOffset>::from(chrono::Utc::now())),
+        None => DateTime::<FixedOffset>::from(chrono::Utc::now()),
+    };
 
     if updated_at.timestamp() + 300 > chrono::Utc::now().timestamp() {
-        return Ok(Response::from_json(&old_downloads)?)
-    } else {
-        let client = Client::new();
-        let url = "https://api.github.com/repos/Valink-Solutions/teller/releases";
-        let resp = match client.get(url)
-            .header("User-Agent", "chunkvault-updater")
-            .send()
-            .await {
-            Ok(resp) => resp,
-            Err(_) => return Response::error("Failed to fetch releases", 500),
-        };
-
-        let releases: Vec<GitHubRelease> = resp.json().await.map_err(|_| "Failed to parse releases")?;
-
-        let total_downloads: i64 = releases.iter()
-            .flat_map(|release| &release.assets)
-            .map(|asset| asset.download_count)
-            .sum();
-
-
-        let response = TotalDownloads {
-            total_downloads: total_downloads,
-            updated_at: DateTime::parse_from_rfc3339(
-                releases[0].published_at.as_str(),
-            ).map_err(|_| "Failed to parse published date")?.to_rfc3339(),
-        };
-
-        kv.put("recent_download_count", &response)?.execute().await?;
-
-        Ok(Response::from_json(&response)?)
+        if let Some(old_downloads) = old_downloads {
+            return Ok(Response::from_json(&old_downloads)?);
+        }
     }
+
+    let client = Client::new();
+    let url = "https://api.github.com/repos/Valink-Solutions/teller/releases";
+    let resp = client.get(url)
+        .header("User-Agent", "chunkvault-updater")
+        .send()
+        .await
+        .map_err(|_| "Failed to fetch releases")?;
+
+    let releases: Vec<GitHubRelease> = resp.json().await.map_err(|_| "Failed to parse releases")?;
+
+    let total_downloads: i64 = releases.iter()
+        .flat_map(|release| &release.assets)
+        .map(|asset| asset.download_count)
+        .sum();
+
+    let new_downloads = TotalDownloads {
+        total_downloads: total_downloads,
+        updated_at: releases[0].published_at.to_string(),
+    };
+
+    if let Ok(kv) = kv {
+        if let Ok(kv_action) = kv.put("recent_download_count", &new_downloads) {
+            let _ = kv_action.execute().await;
+        }
+    };
+
+    Ok(Response::from_json(&new_downloads)?)
 }
 
 
@@ -115,21 +114,22 @@ async fn get_release(_req: worker::Request, ctx: RouteContext<()>) -> Result<Res
         None => return Response::error("Missing current_version", 400),
     };
 
-    let kv = ctx.kv("KV_CHUNKVAULT_DOWNLOADS")?;
+    let kv = ctx.kv("KV_CHUNKVAULT_DOWNLOADS");
 
-    let old_release: RecentRelease = match kv.get("recent_releases").json::<RecentRelease>().await? {
-        Some(value) => {
-            value
-        },
-        None => return Response::error("Failed to fetch recent download count", 500),
+    let mut old_release = if let Ok(kv) = &kv {
+        let old_release: RecentRelease = kv.get("recent_release").json::<RecentRelease>().await.unwrap().unwrap();
+        old_release
+    } else {
+        RecentRelease::default()
     };
 
-    let updated_at = DateTime::parse_from_rfc3339(
-        old_release.updated_at.as_str(),
-    ).map_err(|_| "Failed to parse updated_at")?;
+    let updated_at = match DateTime::parse_from_rfc3339(&old_release.updated_at.as_str()) {
+        Ok(date) => date,
+        Err(_) => DateTime::<FixedOffset>::from(chrono::Utc::now()),
+    };
     
     if updated_at.timestamp() + 300 > chrono::Utc::now().timestamp() {
-        return match parse_releases(old_release.releases, target.to_string(), arch.to_string(), current_version.to_string()).await {
+        return match parse_releases(old_release, target.to_string(), arch.to_string(), current_version.to_string()).await {
             Ok(release) => {
                 let mut response = Response::from_json(&json!(
                     {
@@ -163,9 +163,16 @@ async fn get_release(_req: worker::Request, ctx: RouteContext<()>) -> Result<Res
             Err(_) => return Response::error("Failed to parse releases", 500),
         };
 
-        return match parse_releases(releases, target.to_string(), arch.to_string(), current_version.to_string()).await {
+        old_release.releases = releases;
+
+
+        return match parse_releases(old_release, target.to_string(), arch.to_string(), current_version.to_string()).await {
             Ok(release) => {
-                kv.put("recent_releases", &release)?.execute().await?;
+                if let Ok(kv) = kv {
+                    if let Ok(kv_action) = kv.put("recent_download_count", &release) {
+                        let _ = kv_action.execute().await;
+                    }
+                };
 
                 let mut response = Response::from_json(&json!(
                     {
@@ -186,8 +193,8 @@ async fn get_release(_req: worker::Request, ctx: RouteContext<()>) -> Result<Res
     }
 }
 
-async fn parse_releases(releases: Vec<GitHubRelease>, target: String, arch: String, current_version: String) -> std::result::Result<RecentRelease, String> {
-    let latest_release = match releases.iter().find(|&release| release.tag_name != current_version.to_owned()) {
+async fn parse_releases(releases: RecentRelease, target: String, arch: String, current_version: String) -> std::result::Result<RecentRelease, String> {
+    let latest_release = match releases.releases.iter().find(|&release| release.tag_name != current_version.to_owned()) {
         Some(release) => release,
         None => return Err("No new release found".to_string()),
     };
@@ -197,6 +204,11 @@ async fn parse_releases(releases: Vec<GitHubRelease>, target: String, arch: Stri
     if file_extension.is_empty() || sig_file_extension.is_empty() {
         return Err("Invalid target".to_string());
     }
+
+    let updated_at = match DateTime::parse_from_rfc3339(latest_release.published_at.as_str()) {
+        Ok(date) => date,
+        Err(_) => DateTime::<FixedOffset>::from(chrono::Utc::now()),
+    };
 
     let update_asset = match latest_release.assets.iter().find(|asset| asset.name.ends_with(&file_extension)) {
         Some(asset) => asset,
@@ -237,7 +249,7 @@ async fn parse_releases(releases: Vec<GitHubRelease>, target: String, arch: Stri
         url: download_url,
         signature: signature,
         notes: clean_markdown(&notes),
-        releases: releases,
+        releases: releases.releases,
         updated_at: chrono::Utc::now().to_rfc3339(),
     };
 
@@ -254,26 +266,27 @@ async fn get_download(_req: worker::Request, ctx: RouteContext<()>) -> Result<Re
         None => return Response::error("Missing arch", 400),
     };
 
-    let kv = ctx.kv("KV_CHUNKVAULT_DOWNLOADS")?;
+    let kv = ctx.kv("KV_CHUNKVAULT_DOWNLOADS");
 
-    let old_release: RecentRelease = match kv.get("recent_releases").json::<RecentRelease>().await? {
-        Some(value) => {
-            value
-        },
-        None => return Response::error("Failed to fetch recent releases", 500),
+    let old_release = if let Ok(kv) = &kv {
+        let old_release: RecentRelease = kv.get("recent_releases").json::<RecentRelease>().await.unwrap().unwrap();
+        old_release
+    } else {
+        RecentRelease::default()
     };
 
-    let old_downloads: TotalDownloads = match kv.get("recent_download_count").json::<TotalDownloads>().await? {
-        Some(value) => {
-            value
-        },
-        None => return Response::error("Failed to fetch recent download count", 500),
+    let old_downloads = if let Ok(kv) = &kv {
+        let old_downloads: TotalDownloads = kv.get("recent_total_download").json::<TotalDownloads>().await.unwrap().unwrap();
+        old_downloads
+    } else {
+        TotalDownloads::default()
     };
-
+    
     // If the value is older than 5 minutes, return it else fetch a new value
-    let updated_at = DateTime::parse_from_rfc3339(
-        old_release.updated_at.as_str(),
-    ).map_err(|_| "Failed to parse updated_at")?;
+    let updated_at = match DateTime::parse_from_rfc3339(old_downloads.updated_at.as_str()) {
+        Ok(date) => date,
+        Err(_) => DateTime::<FixedOffset>::from(chrono::Utc::now()),
+    };
 
     let file_extension = get_download_extension(&target, &arch);
 
@@ -306,7 +319,11 @@ async fn get_download(_req: worker::Request, ctx: RouteContext<()>) -> Result<Re
             ).map_err(|_| "Failed to parse published date")?.to_rfc3339(),
         };
 
-        kv.put("recent_download_count", &new_downloads)?.execute().await?;
+        if let Ok(kv) = &kv {
+            if let Ok(kv_action) = kv.put("recent_download_count", &new_downloads) {
+                let _ = kv_action.execute().await;
+            }
+        };
 
         return Response::redirect(download_url)
     } else {
@@ -326,7 +343,6 @@ async fn get_download(_req: worker::Request, ctx: RouteContext<()>) -> Result<Re
             Err(_) => return Response::error("Failed to parse releases", 500),
         };
     
-        
         let latest_release = match releases.iter().max_by(|a, b| {
             let version_a = Version::parse(a.tag_name.trim_start_matches('v')).unwrap_or_else(|_| Version::new(0, 0, 0));
             let version_b = Version::parse(b.tag_name.trim_start_matches('v')).unwrap_or_else(|_| Version::new(0, 0, 0));
@@ -348,9 +364,13 @@ async fn get_download(_req: worker::Request, ctx: RouteContext<()>) -> Result<Re
             Err(_) => return Response::error("Invalid URL", 400),
         };
 
-        let new_release = parse_releases(releases, target.to_string(), arch.to_string(), "0.0.0".to_string()).await?;
-    
-        kv.put("recent_releases", &new_release)?.execute().await?;
+        let new_release = parse_releases(old_release, target.to_string(), arch.to_string(), "0.0.0".to_string()).await?;
+        
+        if let Ok(kv) = &kv {
+            if let Ok(kv_action) = kv.put("recent_releases", &new_release) {
+                let _ = kv_action.execute().await;
+            }
+        };
 
         let total_downloads: i64 = new_release.releases.iter()
             .flat_map(|release| &release.assets)
@@ -363,8 +383,18 @@ async fn get_download(_req: worker::Request, ctx: RouteContext<()>) -> Result<Re
                 new_release.releases[0].published_at.as_str(),
             ).map_err(|_| "Failed to parse published date")?.to_rfc3339(),
         };
+        
+        if let Ok(kv) = &kv {
+            if let Ok(kv_action) = kv.put("recent_total_downloads", &new_downloads) {
+                let _ = kv_action.execute().await;
+            }
+        };
 
-        kv.put("recent_download_count", &new_downloads)?.execute().await?;
+        if let Ok(kv) = &kv {
+            if let Ok(kv_action) = kv.put("recent_releases", &new_release) {
+                let _ = kv_action.execute().await;
+            }
+        };
 
         Response::redirect(download_url)
     }
